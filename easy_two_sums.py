aa2c31328c05509724f03280d106f8d72dfc59fc
Adam del Cano <robotmascot@gmail.com>
2020-04-17 17:36:09 -0400

    Delete easy_two_sums.py

--- easy_two_sums.py
+++
@@ -1,8 +0,0 @@
-
-class Solution:
-    def twoSum(self, nums, target):
-        solutions = [
-            i for i, j in enumerate(nums, 0)
-            if j * -1 + target in
-            [b for a, b in enumerate(nums) if a != i]]
-        return solutions
