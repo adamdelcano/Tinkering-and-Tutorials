65f84c35ae44927209b40852863e7c766bc327b3
= <robotmascot@gmail.com>
2020-04-18 11:49:20 -0400

    Removed cruft

--- isvalid.py
+++ isvalid.py
@@ -2,10 +2,6 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         valid_chars = '([{'
-        if len(s) == 0:
-            return True
-        if s[-1:] in valid_chars:
-            return False
         next_valid = ''
         for letter in s:
             if not (next_valid.endswith(letter) or (letter in valid_chars)):
