700172ff69add7626a8e8752c69c03842ebafffe
= <robotmascot@gmail.com>
2020-04-19 14:02:16 -0400

    Better implementation of what is still a fundamentally incorrect algorithm.

--- stonks.py
+++ stonks.py
@@ -1,15 +1,12 @@
+# for https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        best_profit = []
-        for day, price in enumerate(prices):
-            possible_selling_days = prices[day + 1:]
-            profit = [selling_price - price for selling_price in possible_selling_days]
-            if profit:
-                best_profit.append(max(profit))
-        if best_profit:
-            if max(best_profit) > 0:
-                return max(best_profit)
-            else:
-                return 0
-        else:
-            return 0
+            biggest_profit = 0
+            for day, buy_price in enumerate(prices):
+                profits = [sell_price - buy_price for sell_price in prices[day:]]
+                if max(profits) > biggest_profit:
+                    biggest_profit = max(profits)
+            return biggest_profit
+                        
