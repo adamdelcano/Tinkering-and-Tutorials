17ce3298e84edc7ff48f30e6aeb6c021abc26651
= <robotmascot@gmail.com>
2020-04-19 16:42:44 -0400

    Alright I think this is solid and actually worth looking at.

--- stonks.py
+++ stonks.py
@@ -1,21 +1,23 @@
 # for https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 
+from typing import List
 
-# so the problem is this looks at the whole list for every entry in the list.
-# I need a version without nested loops if I want to look at a huge range
-# I think it makes the most sense to look at the list backward then?
-# OH SHIT I JUST HAD A BREAKTHROUGH
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-            sell_price = 0
-            buy_price = prices[0]
-            best_profit = 0
-            for current_price in prices:
-                profit = current_price - buy_price
-                if profit > best_profit:
-                    best_profit = profit
-                    sell_price = current_price
-                if current_price < buy_price:
-                    buy_price = current_price
-            return best_profit
+    	"""Finds the maximum value obtained from buying a stock and then
+        later (ONLY AFTER BUYING IT) selling it, given list of prices ordered
+        sequentially by date."""
+        if not prices:  # This seems suboptimal but see line 13
+            return 0
+        sell_price = 0
+        buy_price = prices[0]  # This fails if prices = []
+        best_profit = 0
+        for current_price in prices:
+            profit = current_price - buy_price
+            if profit > best_profit:
+                best_profit = profit
+                sell_price = current_price
+            if current_price < buy_price:
+                buy_price = current_price
+        return best_profit
