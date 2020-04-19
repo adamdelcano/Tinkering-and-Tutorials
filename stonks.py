d33f9312294b9de0a7bfce2bd0b5e720f6e9a8e6
= <robotmascot@gmail.com>
2020-04-19 16:31:43 -0400

    This doesn't work on empty list, but does iterate through the list only ones.

--- stonks.py
+++ stonks.py
@@ -1,12 +1,21 @@
 # for https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 
 
+# so the problem is this looks at the whole list for every entry in the list.
+# I need a version without nested loops if I want to look at a huge range
+# I think it makes the most sense to look at the list backward then?
+# OH SHIT I JUST HAD A BREAKTHROUGH
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-            biggest_profit = 0
-            for day, buy_price in enumerate(prices):
-                profits = [sell_price - buy_price for sell_price in prices[day:]]
-                if max(profits) > biggest_profit:
-                    biggest_profit = max(profits)
-            return biggest_profit
-                        
+            sell_price = 0
+            buy_price = prices[0]
+            best_profit = 0
+            for current_price in prices:
+                profit = current_price - buy_price
+                if profit > best_profit:
+                    best_profit = profit
+                    sell_price = current_price
+                if current_price < buy_price:
+                    buy_price = current_price
+            return best_profit
