65a87be8d6adb8b97957f7ae2aa502ed9ee713b8
= <robotmascot@gmail.com>
2020-04-21 12:58:58 -0400

    Addressed Berger's changes

--- stonks.py
+++ stonks.py
@@ -5,19 +5,14 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-    	"""Finds the maximum value obtained from buying a stock and then
+        """Finds the maximum value obtained from buying a stock and then
         later (ONLY AFTER BUYING IT) selling it, given list of prices ordered
         sequentially by date."""
-        if not prices:  # This seems suboptimal but see line 13
-            return 0
-        sell_price = 0
-        buy_price = prices[0]  # This fails if prices = []
+        buy_price = float("inf")
         best_profit = 0
         for current_price in prices:
-            profit = current_price - buy_price
-            if profit > best_profit:
-                best_profit = profit
-                sell_price = current_price
             if current_price < buy_price:
                 buy_price = current_price
+            else:
+                best_profit = max(best_profit, current_price - buy_price)
         return best_profit
