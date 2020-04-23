a4de993e5ab491410b669ab3e56d1bc5a719dfe1
= <robotmascot@gmail.com>
2020-04-23 13:15:27 -0400

    Removed vestigial if/else per Zach's recommendation

--- plusone.py
+++ plusone.py
@@ -10,21 +10,16 @@ class Solution:
         the string into an integer, adding 1 to that, and reversing
         the process, which frankly wasn't even meaningfully slower."""
         zeroes_to_append = 0
-        new_digit = digits.pop() + 1
-        if new_digit < 10:
-            digits.append(new_digit)
-        else:  # if [9, 9, ... 9] there's a chain reaction that needs to happen
-            zeroes_to_append += 1
-            while digits:
-                new_digit = digits.pop() + 1
-                if new_digit < 10:
-                    digits.append(new_digit)
-                    break  # Stops once there's not a 9 at the end
-                else:
-                    zeroes_to_append += 1
-            if not digits:
-                digits = [1]  # If it 999 it's empty now so start at 1
-            while zeroes_to_append > 0:  # replace every 9 we removed with a 0
-                digits.append(0)
-                zeroes_to_append -= 1
+        while digits:
+            new_digit = digits.pop() + 1
+            if new_digit < 10:
+                digits.append(new_digit)
+                break  # Stops whole loop once there's not a 9 at the end
+            else:
+                zeroes_to_append += 1 
+        if not digits:  # If whole list was 9s, it's empty now so need to fix
+            digits = [1]
+        while zeroes_to_append > 0:  # replace every 9 we removed with a 0
+            digits.append(0)
+            zeroes_to_append -= 1
         return digits
