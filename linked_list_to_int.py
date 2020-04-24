f746bc2117d4d62441209c9464772b328d4dc30b
= <robotmascot@gmail.com>
2020-04-24 10:14:05 -0400

    Streamlined code to remove unnecessary bits.

--- linked_list_to_int.py
+++ linked_list_to_int.py
@@ -14,10 +14,8 @@ class Solution:
         The value of each node in th linked list is either 0 or 1. The
         linked list holds the binary representation of a number. Returns
         the decimal value of the number."""
-        reg_list = []
+        num = ''
         while head:
-            reg_list.append(str(head.val))  # has to be better way than this
+            num += str(head.val)
             head = head.next
-        num = ''.join(reg_list)
-        num = int(num, 2)
-        return num
+        return int(num, 2)
