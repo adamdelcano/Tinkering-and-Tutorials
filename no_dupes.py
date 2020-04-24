902a76d73b8857523a64d7fcdee7c3ef07ca88a1
= <robotmascot@gmail.com>
2020-04-24 19:58:29 -0400

    Removes duplicates from sorted array. Areas for improvement: not actually sure only O(1), float(-inf) seems bad.

---
+++ no_dupes.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """Given a sorted array nums, remove the duplicates in-place such that
+        each element appear only once and return the new length."""
+        last_unique = float("-inf")  # Is there a more idiomatic way?
+        index_to_change = 0
+        for num in nums:
+            if num == last_unique:
+                nums[index_to_change] = 'marked'
+            if num > last_unique:
+                last_unique = num
+                nums[index_to_change] = last_unique
+                index_to_change += 1
+        return index_to_change
