ea7ad333765acc8a47b10464d83c2eae0b41b939
= <robotmascot@gmail.com>
2020-05-01 10:39:00 -0400

    This is ungainly, still trying to figure out how to make this work, might backtrack.

--- matrix_cells.py
+++ matrix_cells.py
@@ -5,9 +5,20 @@ from typing import List
 
 class Solution:
     def allCellsDistOrder(self, R: int, C: int, r0: int, c0: int) -> List[List[int]]:
-        matrix = []
-        for row in range(R):  # create the whole matrix from scratch
+        """Given matrix with R rows and C columns, that has cells with integer
+        coordinates (r, c), where 0 <= r < R and 0 <= c < C. Returns
+        coordinates of all cells in the matrix, sorted by their distance from
+        the initial cell, (r0, c0)."""
+        matrix = {}
+        for row in range(R):  # Might be better off still making list
             for column in range(C):
-                matrix.append([row, column])
-        matrix.sort(key=lambda cell: abs(r0 - cell[0]) + abs(c0 - cell[1]))
-        return matrix
+                distance = (abs(r0 - row) + abs(c0 - column))
+                if distance in matrix:
+                    matrix[distance].append([row, column])
+                else:
+                    matrix[distance] = [[row, column]]
+        matrix_list = []
+        for key in sorted((matrix.keys())):  # I know this isn't better
+            for cell in matrix[key]:
+                matrix_list.append(cell)
+        return matrix_list
