7d2897187125005dea8ff7259e85d62a3d3e8355
= <robotmascot@gmail.com>
2020-05-16 16:56:12 -0400

    I might just be dumb and thinking this is easy idk.

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+# for https://leetcode.com/problems/fizz-buzz/
+# I don't know if this is cheating? It seems super easy idk
+
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        fizzBuzzList = []
+        for num in range(1, n + 1):
+            if num % 3 == 0 and num % 5 == 0:
+                fizzBuzzList.extend(['FizzBuzz'])
+            elif num % 3 == 0:
+                fizzBuzzList.extend(['Fizz'])
+            elif num % 5 == 0:
+                fizzBuzzList.extend(['Buzz'])
+            else:
+                fizzBuzzList.append(str(num))
+        return fizzBuzzList
