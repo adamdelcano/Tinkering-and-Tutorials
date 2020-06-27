5ef316c0c7d2d700ea910f8e593ce83bed798311
= <robotmascot@gmail.com>
2020-06-27 00:28:44 -0400

    Cut while loop for enumarating, which lets me cut a variable

--- reshape_the_matrix.py
+++ reshape_the_matrix.py
@@ -7,18 +7,16 @@ class Solution:
     into a different size of r rows and c columns while maintaining the
     elements in row-traversing order. If imposible returns original. """
     def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
-        step = 0
         if len(nums) * len(nums[0]) != (r * c):  # check if reshaping possible
             return nums  # return if not
         # I don't want to admit how long it took me fumbling before just
         # looking up the list flattening comprehension.
         nums = [digit for row in nums for digit in row]
-        # This is inelegant but enumerate seems moreso
-        while r > 0:
+        # Enumerate lets me have an incrementing counter
+        for index, item in enumerate(nums):
             # Convert a column-sized slice of nums into one column
-            nums[step:(c + step)] = [nums[step:(c + step)]]
-            # Step keeps us at the correct starting index
-            step += 1
-            # Decrement r so we end up doing this row number of times
-            r -= 1
+            nums[index:(c + index)] = [nums[index:(c + index)]]
+            # Stop when at breakpoint
+            if index == r:
+                break
         return nums
