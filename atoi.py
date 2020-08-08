df8d7d42094d13519f9c772f034bc092993b330c
= <robotmascot@gmail.com>
2020-08-08 09:39:33 -0400

    Addressed Zach's notes from the night.

--- atoi.py
+++ atoi.py
@@ -19,26 +19,30 @@ class Solution:
         neg_flag = False  # this could be 1 instead and *= -1 to switch
         int_max = 2**31 - 1
         int_min = -2**31
-        started = False
         a = string.lstrip()   # work with a string with removed whitespace
+        try:
+            # Not sure which version is more readable, have commented one out.
+            # They don't seem to make a difference in runtime on leetcode.
+            # I kinda assume the first is slightly longer but still an O(1)
+            # check so not relevant, just a matter of which looks better to
+            # the human eye.
+            #
+            # if a[0] in ('+', '-'):
+            #     if a[0] == '-':
+            #         neg_flag = True
+            #     a = a[1:
+            if a[0] == '-':
+                neg_flag = True
+                a = a[1:]
+            elif a[0] == '+':
+                a = a[1:]
+        except IndexError:
+            return 0
         for char in a:
             # test if char is number, add to i if so
-            if char.isnumeric() and i == 0 and started is False:
-                i = int(char)
-                started = True
-            elif char.isnumeric():
+            if char.isnumeric():
                 i *= 10
                 i += int(char)
-            # special case to handle +/-, can be expanded to handle \t, \r, \n
-            # using an escape flag and some extra elifs
-            elif char.isnumeric() is False and i == 0 and started is False:
-                if char == '-':
-                    started = True
-                    neg_flag = True
-                elif char == '+':
-                    started = True
-                else:
-                    return 0
             # now go until you hit something not an integer
             else:
                 break
