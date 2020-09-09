a5ada854f11cf8f403c6e2723f3a0b57c851beac
= <robotmascot@gmail.com>
2020-09-09 17:02:12 -0400

    First workable version: not good, but takes json data, logs, has VERY basic error handling.

--- aiohttp_demo/sec_process.py
+++ aiohttp_demo/sec_process.py
@@ -1,38 +1,41 @@
 import os
 import pandas as pd
-import json
+from datetime import datetime
 
 current_directory = str(os.getcwd())
 
 
-def extrapolate(earlier, later):
-    """Basic function to perform linear extrapolation of two numbers"""
-    return (later - earlier) + later
-
-
-async def log(data, error=False):
-    """Logs incoming data in data file"""
+async def log(data, message=''):
+    """Logs incoming data in data file, along with optional message"""
     log_file = open((current_directory + '_log.txt'), 'a')
-    if error is True:  # Currently unused
-        log_file.write("ERRONEOUS DATA:", '\n')
+    log_file.write(str(datetime.now()))
+    log_file.write(message)
     log_file.write(str(data))
     log_file.write('\n')
     log_file.close()
 
 
 async def process(data):
-    """Converting incoming data to dataframe"""
+    """Converting incoming data to dataframe. Currently in need of expansion
+    to handle basic data sanitization and error checking."""
     df = pd.DataFrame(data)
     return df
 
 
-async def interpolate(data):
+async def extrapolate(data):
     """Takes the dataframe made by process and adds a new row with
     extrapolated data and returns it."""
     df = await process(data)
-    if df is TypeError:
-        return TypeError
+    # create next day
+    df.index = pd.to_datetime(df.index)  # converts from string to datetime
     next_day = df.index[-1] + pd.Timedelta(1, unit='day')
-    df.loc[next_day] = [i for i in map(extrapolate, df.iloc[-2], df.iloc[-1])]
-    jdf = pd.to_json(df)
-    return jdf
+    # advance until not weekend
+    while next_day.weekday() > 4:
+        next_day += pd.Timedelta(1, unit='day')
+    # append to end of dataframe
+    df.loc[next_day] = None
+    df.index = df.index.astype(str)  # back to string frm datetime
+    # actually interpolate
+    df = df.interpolate(limit_area='outside')
+    await log(df, 'DataFrame:')
+    return df
--- aiohttp_demo/views.py
+++ aiohttp_demo/views.py
@@ -1,17 +1,23 @@
 from aiohttp import web
 import pandas as pd
-from sec_process import log, interpolate
+from sec_process import log, extrapolate
+import json
 
 
 async def index(request):
-    return web.Response(text='This server expects a post of ')
+    return web.Response(text='This server expects a post of json data.')
 
 
 async def forecast(request):
-    data = await request.json()
-    await log(data)
-    new_data = await interpolate(data)
-    if new_data is TypeError:
-        return web.Response(text='Error: data not given as json')
-    else:
-        return web.json_response(new_data, dumps=pd.io.json.dumps)
+    """Takes json post, logs it, processes it, returns it."""
+    try:
+        data = await request.json()
+        await log(data, 'json received:')
+    # initial error handling, is insufficiently robust
+    except json.decoder.JSONDecodeError:
+        await log(request, 'Data received as non-json format:')
+        return web.Response(text="""
+            Error: server was unable to parse request's json.
+            """)
+    new_data = await extrapolate(data)
+    return web.json_response(new_data, dumps=pd.io.json.dumps)
