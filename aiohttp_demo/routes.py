8c5914a09c1fa2ad982be82cbebdc185cfad6b9c
= <robotmascot@gmail.com>
2020-09-12 16:24:40 -0400

    Renaming folder

--- aiohttp_demo/routes.py
+++
@@ -1,7 +0,0 @@
-from views import index, forecast
-
-
-def setup_routes(app):
-    """ Just has route for forecast and a basic index route."""
-    app.router.add_get('/', index)
-    app.router.add_post('/', forecast)
