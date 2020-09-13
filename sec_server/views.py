ea7625a4ed2202767cb1ff04a95afa0cfdc00a4f
= <robotmascot@gmail.com>
2020-09-13 18:03:44 -0400

    Attempting slightly fancier landing page for a sec

--- sec_server/views.py
+++ sec_server/views.py
@@ -6,8 +6,21 @@ import logging
 
 
 async def index(request):
-    logging.info('Received %s non-post request at %(asctime)s.', request)
-    return web.Response(text='This server expects a post of json data.')
+    logging.info('Received %s non-post request', request)
+    return web.Response(text="""<!DOCTYPE html>
+<html>
+  <title>If You're Here, Something Has Gone Wrong</title>
+
+  <h1>Something has gone terribly wrong</h1>
+  <p>Okay, not <i>that</i> wrong.</p>
+  <p>This is just my shoddy static landing page.
+  Right now you want to add '/forecast' to the URL you used to navigate here
+  and then send a POST request containing a json serialized dataframe.</p>
+  <p>Or not, it's not that interesting.</p>
+  <p> </p>
+  <p>Also if you're here because I (Adam) asked you to help me test this,
+   I guess it's actually the <b>opposite</b> of something going wrong?</p>
+</html>""", content_type='text/html')
 
 
 async def forecast(request):
