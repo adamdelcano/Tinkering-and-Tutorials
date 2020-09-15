1376c7aa31ea18471f31a9703d59372b93228510
= <robotmascot@gmail.com>
2020-09-14 23:29:51 -0400

    Decorators is test file

---
+++ decorators.py
@@ -0,0 +1,112 @@
+import functools
+import time
+from random import randint
+import random
+from flask import Flask, g, request, redirect, url_for
+
+
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do something before
+        value = func(*args, **kwargs)
+        # Do something after
+        return value
+    return wrapper_decorator
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()  # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()  # 2
+        run_time = end_time - start_time  # 3
+        print(f'Finished {func.__name__!r} in {run_time:.4f} secs')
+        return value
+    return wrapper_timer
+
+@timer
+def waste_some_time(num_times):
+    for _ in range(num_times):
+        sum([i**2 for i in range(10000)])
+
+
+def debug(func):
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]  # 1
+        kwargs_repr = [f'{k}={v!r}' for k, v in kwargs.items()]  # 2
+        signature = ', '.join(args_repr + kwargs_repr)  # 3
+        print(f'Calling {func.__name__} {signature}')
+        value = func(*args, **kwargs)
+        print(f'{func.__name__!r} returned {value!r}')   # 4
+        return value
+    return wrapper_debug
+
+@debug
+def make_greeting(name, age=None):
+    if age is None:
+        return f'Howdy {name}!'
+    else:
+        return f'Whoa {name}! {age} already, you\'re growing up!'
+
+def slow_down(func):
+    """Sleep 1 second before calling the function"""
+    @functools.wraps(func)
+    def wrapper_slow_down(*args, **kwargs):
+        time.sleep(1)
+        return func(*args, **kwargs)
+    return wrapper_slow_down
+
+@slow_down
+def countdown(from_number):
+    if from_number < 1:
+        print("Liftoff!")
+    else:
+        print(from_number)
+        countdown(from_number - 1)
+
+
+PLUGINS = dict()
+def register(func):
+    """Register a function as a plug-in"""
+    PLUGINS[func.__name__] = func
+    return func
+
+@register
+def say_hello(name):
+    return f"Hello {name}"
+
+@register
+def be_awesome(name):
+    return f"Yo {name}, together we are the awesomest!"
+
+def randomly_greet(name):
+    greeter, greeter_func = random.choice(list(PLUGINS.items()))
+    print(f"Using {greeter!r}")
+    return greeter_func(name)
+
+app = Flask(__name__)
+
+def login_required(func):
+    """Make sure user is logged in before proceeding"""
+    @functools.wraps(func)
+    def wrapper_login_required(*args, **kwargs)):
+        if g.user is None:
+            return redirect(url_for("login", next=request.url))
+        return func(*args, **kwargs)
+    return wrapper_login_required
+
+@app.route("/secret")
+@login required
+def secret():
+    pass
+
+
+
+def main():
+    return make_greeting('Adam', 35)
+
+if __name__ == '__main__':
+    main()
