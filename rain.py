d467a33e760df5c749b9a35c47e9088ad1943487
= <robotmascot@gmail.com>
2020-11-04 19:57:45 -0500

    Ashamed to admit I looked up the answer to the algorithm that would solve this, extra ashamed because it was just 'DO THE LAST ON AGAIN BUT WITH SLIGHTLY DIFFERENT MATH' but I did it.

--- rain.py
+++ rain.py
@@ -4,15 +4,47 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        last_wall = 0
-        total_trapped_water = 0
-        current_trapped_water = 0
-        for index, wall in enumerate(height):
-            trapped_water = last_wall - wall
-            if trapped_water > 0:
-                current_trapped_water += trapped_water
-            else:
-                last_wall = wall
-                total_trapped_water += current_trapped_water
-                current_trapped_water = 0
-        return total_trapped_water
+        """
+        Given n non-negative integers representing an elevation map where the
+        width of each bar is 1, compute how much water it can trap after
+        raining.
+        """
+        if not height:
+            return 0
+        left_cursor = {
+            "index": -1,
+            "max_height": 0,
+            "height": 0
+        }
+        right_cursor = {
+            "index": (len(height) - 1),
+            "max_height": height[-1],
+            "height": height[-1]
+        }
+        rain = 0
+        while left_cursor["index"] != right_cursor["index"]:
+            if left_cursor["height"] < right_cursor["height"]:
+                left_cursor["index"] += 1
+                left_cursor["height"] = height[left_cursor["index"]]
+                if left_cursor["height"] > left_cursor["max_height"]:
+                    left_cursor["max_height"] = left_cursor["height"]
+                elif left_cursor["height"] < right_cursor["max_height"]:
+                    rain += (
+                        min(
+                            left_cursor["max_height"],
+                            right_cursor["max_height"]
+                        ) - left_cursor["height"]
+                    )
+            elif right_cursor["height"] <= left_cursor["height"]:
+                right_cursor["index"] -= 1
+                right_cursor["height"] = height[right_cursor["index"]]
+                if right_cursor["height"] > right_cursor["max_height"]:
+                    right_cursor["max_height"] = right_cursor["height"]
+                elif right_cursor["height"] < left_cursor["max_height"]:
+                    rain += (
+                        min(
+                            left_cursor["max_height"],
+                            right_cursor["max_height"]
+                        ) - right_cursor["height"]
+                    )
+        return rain
