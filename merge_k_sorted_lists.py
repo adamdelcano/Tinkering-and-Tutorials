910efa93d00fc948905835c96bc5b427b377e266
= <robotmascot@gmail.com>
2020-11-11 16:42:25 -0500

    saving work, technically works, not done

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -1,5 +1,3 @@
-# https://leetcode.com/problems/merge-k-sorted-lists/
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,4 +5,24 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        
+        lists = [node for node in lists if node]
+        lists.sort(key=lambda node:node.val)
+        if not lists:
+            return None
+        merge_head = lists[0]
+        merge_tail = merge_head
+        try:
+            lists[0] = lists[0].next
+        except AttributeError:
+            pass
+        while lists:
+            lists = [node for node in lists if node]
+            if not lists:
+                break
+            lists.sort(key=lambda node:node.val)
+            new_tail = ListNode(lists[0].val, next=None)
+            merge_tail.next = new_tail
+            merge_tail = new_tail
+            lists[0] = lists[0].next
+        return merge_head
+                
