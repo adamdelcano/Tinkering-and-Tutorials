988cdcc29071a6739c8dbe02de98f763256ef4d9
= <robotmascot@gmail.com>
2020-11-29 12:18:19 -0500

    updated checking the window to use pandas and therefore work on business days

--- sec_server/yfinance_process.py
+++ sec_server/yfinance_process.py
@@ -1,10 +1,10 @@
 from datetime import date
-from datetime import timedelta
 
 import pandas as pd
 import yfinance as yf
 
-def get_history(stock: str, window: int) -> pd.DataFrame:
+
+async def get_history(stock: str, window: int) -> pd.DataFrame:
     """
     Retrieves stock ticker history for the last window of time.
 
@@ -16,16 +16,9 @@ def get_history(stock: str, window: int) -> pd.DataFrame:
     and an int window which represents a number of days, and returns a pandas
     dataframe.
     """
+    window += 1
     stock_obj = yf.Ticker(stock)
     today = date.today()
-    # realized as i finished for the day that this doesn't handle date right
-    # since it's tracking calendar and not business days, need to fix that
-    # probably just using the pandas stuff from last time
-    duration = timedelta(days=window)
-    starting = today - duration
-    return stock_obj.history(start=starting, end=today)
-
-
-if __name__ == '__main__':
-    test = get_history('mrna', 30)
-    print(test)
+    # window + 1 here so that the range is actually len(window)
+    starting_point = today - pd.tseries.offsets.BDay(window + 1)
+    return stock_obj.history(start=starting_point, end=today)
